23bf979b2b49503825274171397801b509a03fc5
saindou kaniza <saindoukaniza56@gmail.com>
2024-11-15 11:37:23 +0300

    app.js server.js

--- app.js
+++ app.js
@@ -1,14 +1,82 @@
 // Ici, j'ai import du frameword ExpressJs
-// j' import express from "express";
+// j'import express from "express";
 const express = require ("express");
 
 // je crée l'application ExpressJs
 const app = express();
 
+// Ici, le serveur il va retourne la réponse ci-dessous peu importe la requête
+// qu'on va lui donnée c'est "même" la reponse par défaut.
+/*app.use((req, res)=> {
+    res.json({message : "Votre messsage a bien été reçu"});
+});*/
+
+// Ici, j'import l'application "app".
+module.exports = app;
+
+
 // Ici,je définit une route de type GET
+// Ici, on gére les requête GET vers la page d'accueil
 app.get("/", (req, res) => {
     res.end("Bonjour, je suis le nouveau serveur");
-}) ;
+});
 
-//je crée la route "/Bonjour" de type
-// gére les requête GET vers
+//je veux crée la route "/Bonjour" de type GET.
+// qui va gére les requête GET vers le navigateur qui va s'affichier "Bonjour".
+app.get("/", (req, res) => {
+    res.end("hello, how are you")
+});
+
+// je veux crée un route "/cusine" de type GET
+// qui va gére les requête GET vers, le navigateur qui va s'affichier "Cusine".
+app.get("/cusine", (req, res) => {
+    res.end(" Agnati bangani.");
+});
+
+// ici, je crée une route "/bangalo" de type GET
+// pour voir une liste de bangalo.
+app.get("/bangalo", (req, res) => {
+    res.end("vous êtes dans le bangalo");
+});
+
+
+
+app.listen(3001, () =>{
+    console.log("serveur écoute le port 3001");
+});
+ 
+
+// Ici, je crée un route "/accueil" de type GET.
+app.get("/accueil", (req, res) => {
+    res.end("Bienvenu dans notre page d'accuiel.");
+// ici, il va gére les requête GET vers, qui va s'affichier "Bienvenu etc..".
+});
+
+// Ici, je crée une route en utilisant la méthode de type "GET".
+app.get("/apropos", (req, res) => {
+    res.end(" Vous etre bien a notre page de presentation");
+    // et la je fait "res.end" pour affiché le méssage "vous etre bien etc.."
+});
+
+app.get("/services", (req, res) => {
+    res.end("Nos services est les meilleurs du mode");
+});
+
+app.get("/contact", (req, res) => {
+    res.end("A nous contact");
+});
+
+
+    // Ici, je crée une route en utilisant la méthode de type "POST"
+app.post("/email", (req, res)=> {
+    res.end("notre email");
+     // ici, je fait "res.end" pour afficher le message "Notre email".
+});
+
+app.put("/telephone", (req, res) => {
+    res.end("voici notre numero de telephone");
+});
+
+app.delete("/Bonjour", (req, res) => {
+    res.end("Bonjour bienvenu a notre page web");
+});
--- server.js
+++ server.js
@@ -1,27 +1,55 @@
 // Ici, j'importer le protocoé le http.
 // Ici, le require() permet d'importer le package,
 // Ici, le package import est http.
-// avant de convertir en ES, j'avais écrit :
+// avant on convertir en ES, j'avais écrit :
 // const http = require("http"); => code selon ES6 (avant 2015).
-import { createServer } from "http";// co dage selon ES6 (2015)
+//import { createServer } from "http"; // co dage selon ES6 (2015)
 
+// Ici,j'importe le module "http"
+const http = require("http");
+const app = require("./app");
+
+// je déclare mon variable "3002"
+const numPort = 3002;
+
+
+app.set("port", numPort);
+// Ici, je veux  crée le serveur
+const server = http.createServer(app);
+
+// Ici, je veus récupére l'heure ici
+// Ici, je crée nouvelle instance
+const date = new Date();
+// Ici, je déclare le varible "Heure"
+const heure = date.getHours();
+// ici,je déclare le variable "Minutes"
+const minutes = date.getMinutes();
+
+server.listen(numPort, () =>{
+    //console.log('Le serveur est activé! au port : ,${numPort}');
+    console.log("Le serveur est activé au port :", numPort);
+    // Ici, j'afficher heur et minutes.
+    //console.log(heur+ ":" +minutes)
+    console.log(date.toLocaleDateString(), " ", date.toLocaleTimeString())
+});
 
 
 // Ici je déclare le serveur en utilisant http.
 // const server = http.createServer((req,res) => {});
 // je crée le serveur selon la recommandation ES6.
-const server1 = createServer(app);
+//const server1 = createServer(app);
 
 
- const server = createServer((req, res) => {
+
+ /*const server = createServer((req, res) => {
     // pourvoir retourné la réponse, je renvoie au client le message "Bonjour ..."
     // pour renvoie le message de réponse,"res" utilise la méthode "end()".
     res.end("Bonjour, je suis le serveur.");
 
-});
+});*/
 
 // Ici, le serveur est disponible sur le port 3000.
-server.listen(3000);
+//server.listen(3000);
 
 /* Ici, je vais lancer le server sur GitBash,est j'exécuté la commande "node server".
 sur le navigateur web, je vais tapez "localhost:3000".
